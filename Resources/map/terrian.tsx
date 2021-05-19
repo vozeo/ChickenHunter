e9ef878497bf1deb9b9c27ba66e9dfec3897a350
Baneist <245882079@qq.com>
2021-05-19 19:15:32 +0800

    旧地图删除
    删除旧地图 新地图稍后传上来

--- Resources/map/terrian.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="terrian" tilewidth="32" tileheight="32" tilecount="2560" columns="64">
- <image source="terrian.png" width="2048" height="1280"/>
-</tileset>
--- Resources/map/tree01.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="trees" tilewidth="135" tileheight="145" tilecount="5" columns="5">
- <image source="tree01.png" width="675" height="145"/>
-</tileset>
