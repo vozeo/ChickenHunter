514cbb81a775ca468e57ba31299369d6e88d67ba
Baneist <38121242+Baneist@users.noreply.github.com>
2021-06-12 22:53:55 +0800

    Update Server.cpp

--- Classes/Network/Server.cpp
+++ Classes/Network/Server.cpp
@@ -18,6 +18,7 @@ bool CHServer::delete_uid(int id)
     if(uid_usage[id] == false)
         return false;
     uid_usage[id] = false;
+    return true;
 }
 
 CHServer::CHServer(const char* ip, unsigned short port)
