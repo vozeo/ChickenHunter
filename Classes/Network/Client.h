c5f4b06f2bd8f5508ee0f1f00e9fef86cd2dcf6b
Baneist <38121242+Baneist@users.noreply.github.com>
2021-06-12 17:51:23 +0800

    完成client和传输类的拟定

--- Classes/Network/Client.h
+++ Classes/Network/Client.h
@@ -1,4 +1,28 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include <string>
 #include "yasio/yasio.hpp"
+#include "Network/TransStructure.h"
+
 using namespace yasio;
 using namespace yasio::inet;
-int makeConnection();
+
+class CHClient
+{
+	io_service* client;
+	transport_handle_t thandle;
+	int uid = 0;
+public:
+	RoomInformation room;
+	MapInformation map;
+	
+public:
+	CHClient(const char* ip, unsigned short port = 25595);
+	bool link();
+	int getuid();
+	void setName(const char* name);
+	bool upload(PlayerAction action);
+};
+
+#endif 
