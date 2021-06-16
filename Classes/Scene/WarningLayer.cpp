b0136d18ccf4e9424f9c612da6abdf6f02fabc28
Vozeo <2488684796@qq.com>
2021-06-16 20:36:55 +0800

    Add warning layer.

---
+++ Classes/Scene/WarningLayer.cpp
@@ -0,0 +1,42 @@
+#include "Scene/WarningLayer.h"
+
+USING_NS_CC;
+
+bool WarningLayer::init()
+{
+    if (!Layer::init())
+    {
+        return false;
+    }
+
+	Sprite* back = Sprite::create("images/notice.png");
+	addChild(back, 0);
+	back->setPosition(Vec2(winSize.width / 2, winSize.height / 2));
+
+	volumeSlider = Slider::create();
+	volumeSlider->loadBarTexture("images/sliderBack.png");
+	volumeSlider->loadSlidBallTextures("images/sliderNode.png", "images/sliderNode.png", "images/sliderNode.png");
+	volumeSlider->loadProgressBarTexture("images/sliderBar.png");
+
+	volumeSlider->addTouchEventListener([=](Ref* sender, Widget::TouchEventType type) {
+		if (type == ui::Widget::TouchEventType::ENDED)
+			M_Volume = static_cast<float>(volumeSlider->getPercent()) / 100.0f;
+		});
+	volumeSlider->setPosition(Vec2(winSize.width / 2, winSize.height / 1.7f));
+	volumeSlider->setPercent(static_cast<int>(M_Volume * 100.0f));
+	this->addChild(volumeSlider, 1);
+	
+	auto choiceBack = MenuItemFont::create("Back", [=](Ref* render) {
+		removeFromParent();
+		});
+	auto menu = Menu::create();
+	menu->addChild(choiceBack);
+	menu->setPosition(winSize.width / 2, winSize.height / 2.7f);
+	addChild(menu, 1);
+
+	return true;
+}
+
+void WarningLayer::closeCallback(cocos2d::Ref* pSender) {
+	removeFromParent();
+}
